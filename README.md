31392443bd5aac10fdfd3eb2f7ed0c4634f80e01
Olga Shkola <shkola.olga@gmail.com>
2023-11-14 12:04:46 +0300

    Delete fids-env directory

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # PythonScripts
+Проект скриптов на Python
+
+## Подготовка к запуску и запуск
+```bash
+$ virtualenv -ppython3 <имя репозитория>-env
+$ source ./<имя репозитория>-env/bin/activate
+$ cd <имя репозитория> # переходим в корень дерева исходных текстов проекта
+$ pip install -r requirements.txt
+```
--- fids/fid.py
+++ fids/fid.py
@@ -6,40 +6,28 @@ def get_result(file):
     list_id = soup.select('offer')
     code_list = []
     url_list = []
-    code_set = ()
-    url_set = ()
+    print(file)
+    print(len(list_id))
     for id in list_id:
         code = id.attrs['id']
         code_list.append(code)
-        code_set = code_set + (code,)
         url = id.url
         url_list.append(url)
-        url_set = url_set + (url,)
         # name = id.select_one('name').text.split(' ')[0]
         name = id.select_one('name').text
-        print(code, name)
+        # print(code, name)
+        available = id.attrs['available']
+        if available == 'true':
+            continue
+        else:
+            print(code, available)
 
 
     # print(len(code_list))
-    # print(len(code_set))
-    # print(len(code_list) == len(code_set))
+    # print(len(set(code_list)))
+    # print(len(code_list) == len(set(code_list)))
     # print(len(url_list))
-    # print(len(url_set))
-    # print(len(url_list) == len(url_set))
-    # return code_list, code_set
+    # print(len(set(url_list)))
+    # print(len(url_list) == len(set(url_list)))
     # print(list_id)
     # return summ_result(list_id)
-
-
-def summ_result(list_id):
-    result_list = ''
-    result_list = result_list + list_id
-    print(len(result_list))
-#
-#
-#     url_summ = code_set + url_set
-#     print(print(len(code_list) == len(code_set)))
-#     print(len(url_list) == len(url_set))
-
-
-
